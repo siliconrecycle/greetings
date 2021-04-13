2432f5265cb9aae3867964569efcac3f8c27f7ad
Qian <2252292157@qq.com>
2021-04-13 15:50:41 +0800

    mymodule: changes for v0.1.0

---
+++ greetings.go
@@ -0,0 +1,44 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Hello(name string) (string, error){
+	// 如果没有输入名字，则返回错误提示提示。
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat())
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"你好，欢迎你的到来, %v ！",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
